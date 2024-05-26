095bd90fbcf40e1033afd34e668d4149ab59f09d
gagadesu <16465863+gagadesu@users.noreply.github.com>
2024-05-26 16:53:00 +0800

    style: Remove unused tmux weather location script

--- home/dot_config/tmux/scripts/executable_set_tmux_weather_location.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# fetch city name
-city=$(curl -s http://myip.ipip.net | awk '{print $5}')
-
-# set city
-if [ -n "$city" ]; then
-	tmux set-option -g @tmux-weather-location "$city"
-fi
